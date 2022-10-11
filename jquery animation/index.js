0da2cd6bc53baa909d433f98add48df7e8f7f8d4
Moklasur Rahamn <68967224+moklasurrahman@users.noreply.github.com>
2022-10-11 12:15:34 -0700

    Add files via upload

---
+++ jquery animation/index.js
@@ -0,0 +1,13 @@
+$("#btn").click(function () {
+    // $(".item").show(1000).hide(10000)
+    // $(".item").toggle(2000)
+    //  $(".item").fadeToggle(2000)
+    //  $(".item").fadeTo(2000, 0.5) opacity
+
+    //  $(".item").slideToggle(2000)
+
+    $(".item").animate({opacity:"0.5", width:"500px"},2000)
+
+    
+    
+})
